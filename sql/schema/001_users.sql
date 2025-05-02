8716e10cd4cbc670859de2deba1d2534160653ec
“hectorsvill” <hectorstevenvillasano@gmail.com>
2025-05-02 19:59:09 -0400

    goose up, implemented postgresql uuid function

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,12 +1,13 @@
 -- +goose Up
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE users (
-    id: uuid DEFAULT uuid_generate_v4 (),
-    created_at: TIMESTAMP,
-    updated_at: TIMESTAMP,
-    name: VARCHAR NOT NULL,
-    PRIMARY KEY (id),
-)
+    id uuid DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+);
 
 -- +goose Down
 DROP TABLE users;
-
