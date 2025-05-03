e47f0e1d28867ff455d108d4deda4d72dfa5aeef
“hectorsvill” <hectorstevenvillasano@gmail.com>
2025-05-03 13:49:30 -0400

    func handlerRegister(s *state, cmd command) error

--- handler_user.go
+++ handler_user.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os/user"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+
+	"github.com/hectorsvill/gator/internal/database"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -16,5 +23,24 @@ func handlerLogin(s *state, cmd command) error {
 	}
 
 	fmt.Println("User switched successfully!")
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("ussage: %s <name>", cmd.Name)
+	}
+
+	name := cmd.Args[0]
+
+	err := s.cfg.SetUser(name)
+	if err != nil {
+		return fmt.Errorf("%s", err)
+	}
+
+	ctx := context.Background()
+	uuidStr := uuid.NewString()
+
+
 	return nil
 }
