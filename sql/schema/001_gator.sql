c99dc3f41352e0ee920400040815f76bda352d22
“hectorsvill” <hectorstevenvillasano@gmail.com>
2025-05-07 22:55:09 -0400

    update schema

--- sql/schema/001_gator.sql
+++ sql/schema/001_gator.sql
@@ -1,4 +1,3 @@
-
 -- +goose Up
 CREATE SCHEMA gator;
 
@@ -18,6 +17,17 @@ CREATE TABLE gator.feeds (
     user_id UUID NOT NULL UNIQUE REFERENCES gator.users (id) ON DELETE CASCADE
 );
 
+CREATE TABLE gator.feed_follows (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    feed_id UUID NOT NULL REFERENCES feeds(id) ON DELETE CASCADE,
+    UNIQUE (user_id, feed_id)
+);
+
 -- +goose Down
-DROP TABLE users;
-DROP TABLE feeds;
+DROP TABLE gator.users;
+DROP TABLE gator.feeds;
+DROP TABLE gator.feed_follows;
+DROP SCHEMA gator;
