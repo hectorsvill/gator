b901a8474e7de80d3ec041d29792d0b0cf1c9808
“hectorsvill” <hectorstevenvillasano@gmail.com>
2025-05-10 08:39:56 -0400

    fix column issue: add all columns explicitly

--- sql/queries/feed_follows.sql
+++ sql/queries/feed_follows.sql
@@ -13,13 +13,21 @@ INNER JOIN gator.feeds ON inserted_feed_follow.feed_id = gator.feeds.id
 INNER JOIN gator.users ON inserted_feed_follow.user_id = gator.users.id;
 --
 
+
 -- name: GetFeedFollowsForUser :many
-SELECT gator.feed_follows.*, gator.feeds.name AS feed_name, gator.users.name AS user_name
+SELECT 
+    gator.feed_follows.feed_id,
+    gator.feed_follows.user_id,
+    gator.feed_follows.created_at,
+    gator.feeds.name AS feed_name, 
+    gator.users.name AS user_name
 FROM gator.feed_follows
-INNER JOIN gator.feeds ON gator.feed_follows.feed_id = feeds.id
-INNER JOIN gator.users ON gator.feed_follows.user_id = users.id
+INNER JOIN gator.feeds ON gator.feed_follows.feed_id = gator.feeds.id
+INNER JOIN gator.users ON gator.feed_follows.user_id = gator.users.id
 WHERE gator.feed_follows.user_id = $1;
 --
+
+
 
 
 
