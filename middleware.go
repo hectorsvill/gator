1670c7d0ec6fb4d86d1d306ce7cd09d4f9f1e6d6
“hectorsvill” <hectorstevenvillasano@gmail.com>
2025-05-20 23:06:43 -0400

    middlewareLoggedIn

---
+++ middleware.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"context"
+
+	"github.com/hectorsvill/gator/internal/database"
+)
+
+func middlewareLoggedIn(handler func(s *state, cmd command, user database.GatorUser) error) func(*state, command) error {
+	return func(s *state, cmd command) error {
+		u, err := s.db.GetUser(context.Background(), s.cfg.UserName)
+		if err != nil {
+			return err
+		}
+		return handler(s, cmd, u)
+	}
+}
